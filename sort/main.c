6856729c5862ae1e9d1586ccabd01029baaaac3f
20161104568 <a20161104568@mac25.local>
2017-06-13 10:50:25 +0800

    sort
    数字排序

--- sort/main.c
+++ sort/main.c
@@ -6,10 +6,22 @@
 //  Copyright © 2017年 20161104568. All rights reserved.
 //
 
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#include<stdio.h>
+int main()
+{
+    int a[10],i,j,t;
+    printf("Input 10 numbers:\n");
+    for(i=0;i<10;i++)
+        scanf("%d",&a[i]);
+    printf("\n");
+    for(i=0;i<9;i++)
+        for(j=0;j<9-i;j++)
+            if(a[j]>a[j+1])
+            {
+                t=a[j];a[j]=a[j+1];a[j+1]=t;
+            }
+    printf("The sorted numbers:\n");
+    for(i=0;i<10;i++)
+        printf("%d",a[i]);
     return 0;
 }
